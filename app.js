5aa90c969cb09c0ba4fde29ed991b854eaa769a7
prince-sunsara <princesunasara977@gmail.com>
2023-06-03 01:06:03 +0530

    database related

--- app.js
+++ app.js
@@ -3,7 +3,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-var _ = require('lodash');
+const _ = require('lodash');
+const mongoose = require("mongoose");
+
+// mongoose.connect("mongodb+srv://prince:<password>@cluster0.bbiddqc.mongodb.net/blogDB");
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -13,23 +17,33 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Global Array
-const composePostsRequests = [];
+// const composePostsRequests = [];
+
+// mongodb schema
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+// mongoDB model
+const Post = new mongoose.model("Post", postSchema);
 
 //  REQUESTS POST | GET
 app.get("/", (req, res) => {
-  res.render("home",{homeContent: homeStartingContent, homePagePost: composePostsRequests});
-});
 
-app.get("/about", (req, res) => {
-  res.render("about", {aboutPageContent: aboutContent});
-});
-
-app.get("/contact", (req, res) => {
-  res.render("contact", {contactPageContent: contactContent});
+  Post.find({}).then((posts) => {
+    console.log("Successfully add data in blogDB");
+    res.render("home", {
+      homeContent: homeStartingContent,
+      posts: posts
+    });
+  }).catch((err) => {
+      console.log(err);
+  });
 });
 
 app.get("/compose", (req, res) => {
@@ -37,27 +51,46 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  const composePost = {
+  const post = new Post({
     title: req.body.composeTitle,
     content: req.body.composeText
-  };
-  composePostsRequests.push(composePost);
-  res.redirect("/");
+  });
+
+  // composePostsRequests.push(composePost);
+  post.save().then(() => {
+    res.redirect("/");
+  }).catch((err) => {
+      console.log(err);
+  });
 });
 
-app.get("/posts/:postName", (req, res) => {
-  const requestedTitle = req.params.postName;
-  composePostsRequests.forEach((post) => {
-    const storedTitle = post.title;
-    const storedContent = post.content;
+app.get("/posts/:postId", (req, res) => {
+  const requestedPostId = req.params.postId;
+  // composePostsRequests.forEach((post) => {
+  //   const storedTitle = post.title;
+  //   const storedContent = post.content;
 
-    if (_.lowerCase(requestedTitle) === _.lowerCase(storedTitle)) {
-      res.render("post", {
-        title: storedTitle,
-        content: storedContent
-      });
-    } 
-  });
+  //   if (_.lowerCase(requestedTitle) === _.lowerCase(storedTitle)) {
+  //     res.render("post", {
+  //       title: storedTitle,
+  //       content: storedContent
+  //     });
+  //   } 
+  // });
+  Post.find({ _id: requestedPostId }).then((post) => {
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  })
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { aboutPageContent: aboutContent });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactPageContent: contactContent });
 });
 
 module.exports = app;
